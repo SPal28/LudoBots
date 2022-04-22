36c2172969b73a50bb560faeac6a52abca5441a1
SPal28 <ps247427@cpslions.com>
2022-04-22 14:49:34 -0400

    Added box.sdf

--- generate.py
+++ generate.py
@@ -1,12 +1,4 @@
-import time
-import pybullet as p
-physicsClient = p.connect(p.GUI)
-for i in range (1000):
-    print(i)
-    time.sleep(1/60)
-    p.stepSimulation()
 import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("box.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
 pyrosim.End()
-p.disconnect()
