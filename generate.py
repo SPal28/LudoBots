80705b03313e9a67d128f5371d67714f38a41c4e
SPal28 <ps247427@cpslions.com>
2022-05-21 17:25:31 -0400

    3 nested for loops

--- generate.py
+++ generate.py
@@ -6,15 +6,20 @@ height = 1
 x = 0.5
 y = 0.5
 z = 0.5 
-for value in range (5):
+for x in range (0, 5):
     x = x + 1
     length = 1
     width = 1
     height = 1
-    for value in range (10):
-        pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-        z = z + 1
-        length = 0.9 * length
-        width = 0.9 * width
-        height = 0.9 * height
+    for y in range (0, 5):
+        y = y + 1
+        length = 1
+        width = 1
+        height = 1
+        for z in range (0, 10):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+            z = z + 1
+            length = 0.9 * length
+            width = 0.9 * width
+            height = 0.9 * height
 pyrosim.End()
