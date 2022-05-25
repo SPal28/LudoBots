14f815847a520931f8470b4d5eae69e364f5db67
SPal28 <ps247427@cpslions.com>
2022-05-25 14:46:36 -0400

    3x3 pyramid

--- generate.py
+++ generate.py
@@ -3,22 +3,16 @@ pyrosim.Start_SDF("boxes.sdf")
 length = 1
 width = 1
 height = 1
-x = 0.5
-y = 0.5
-z = 0.5 
-for x in range (0, 5):
-    x = x + 1
+for x in range (0, 3):
     length = 1
     width = 1
     height = 1
-    for y in range (0, 5):
-        y = y + 1
+    for y in range (0, 3):
         length = 1
         width = 1
         height = 1
-        for z in range (0, 10):
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-            z = z + 1
+        for z in range (0, 3):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z + 0.5] , size=[length,width,height])
             length = 0.9 * length
             width = 0.9 * width
             height = 0.9 * height
