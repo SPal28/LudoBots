fe8ab59b51f1a613c4a28d75a326914cdb8eef55
SPal28 <ps247427@cpslions.com>
2022-06-19 19:07:42 -0400

    Added new brach: joints

--- generate.py
+++ generate.py
@@ -3,17 +3,8 @@ pyrosim.Start_SDF("boxes.sdf")
 length = 1
 width = 1
 height = 1
-for x in range (0, 3):
-    length = 1
-    width = 1
-    height = 1
-    for y in range (0, 3):
-        length = 1
-        width = 1
-        height = 1
-        for z in range (0, 3):
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z + 0.5] , size=[length,width,height])
-            length = 0.9 * length
-            width = 0.9 * width
-            height = 0.9 * height
+x=0
+y=0
+z=1.5
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 pyrosim.End()
