6ada7315d7465f1209561b45ee3cc9a0ad6e9d56
SPal28 <ps247427@cpslions.com>
2022-06-21 09:13:30 -0400

    read a file

--- simulate.py
+++ simulate.py
@@ -5,6 +5,11 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+planeId = p.loadURDF("body.urdf")
+    with open('body.urdf') as f:
+    contents = f.readlines() 
+    # Opened the file and read it 
+    # How do I read this line into an object?
 p.loadSDF("world.sdf")
 for i in range (2000):
     print(i)
