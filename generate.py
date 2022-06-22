6a31736fce7149de0e25ff07a437fa82bb6a3369
SPal28 <ps247427@cpslions.com>
2022-06-22 14:00:36 -0400

    Created a link

--- generate.py
+++ generate.py
@@ -2,9 +2,9 @@ import pyrosim.pyrosim as pyrosim
 length = 1
 width = 1
 height = 1
-x=0
-y=0
-z=1.5
+x=1
+y=1
+z=0.5
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
--- simulate.py
+++ simulate.py
@@ -6,10 +6,13 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 planeId = p.loadURDF("body.urdf")
-    with open('body.urdf') as f:
-    contents = f.readlines() 
+def robotId():
+    open('body.urdf').readlines()
+robotId()
+    # Joints: Step 18
     # Opened the file and read it 
     # How do I read this line into an object?
+    # Should I call this function back too?
 p.loadSDF("world.sdf")
 for i in range (2000):
     print(i)
