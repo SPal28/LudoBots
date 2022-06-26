483a5c35ad4830b308505507bbd9a649b692af76
SPal28 <ps247427@cpslions.com>
2022-06-26 08:55:42 -0400

    Torso and leg

--- generate.py
+++ generate.py
@@ -2,8 +2,8 @@ import pyrosim.pyrosim as pyrosim
 length = 1
 width = 1
 height = 1
-x=1
-y=1
+x=0
+y=0
 z=0.5
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -13,5 +13,8 @@ def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
     pyrosim.End()
-Create_World()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Leg", pos=[x+1,y,z+1] , size=[length,width,height])
+    pyrosim.End()
 Create_Robot()
