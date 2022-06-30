08f488eaab320ef568214a48e8ce2e1f12dbc583
SPal28 <ps247427@cpslions.com>
2022-06-30 11:00:34 -0400

    Added a third link

--- generate.py
+++ generate.py
@@ -7,8 +7,10 @@ y=0
 z=0.5
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute" , position = [x + 0.5, y , z + 0.5])
-    pyrosim.Send_Cube(name="Leg", pos=[x + 1, y,z + 1] , size=[length,width,height])
+    pyrosim.Send_Cube(name="Link0", pos=[x, y, z] , size=[length, width, height])
+    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute" , position = [x , y , z + 0.5])
+    pyrosim.Send_Cube(name="Link1", pos=[x , y, z] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Link2_Link1" , parent= "Link2" , child = "Link1" , type = "revolute" , position = [x , y , z + 0.5])
+    pyrosim.Send_Cube(name="Link2", pos=[x, y, z] , size=[length, width, height])
     pyrosim.End()
 Create_Robot()
