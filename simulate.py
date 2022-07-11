7e21e1ed414127b5d0e8bcfd8bee048da49a55c3
SPal28 <ps247427@cpslions.com>
2022-07-11 11:34:03 -0400

    Adding back leg sensor

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet_data
 import time
 import pybullet as p
+import pyrosim.pyrosim as pyrosim
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -10,8 +11,12 @@ robotId = p.loadURDF("body.urdf")
     # open('body.urdf').readlines()
 # robotId()
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range (1000):
     print(i)
     time.sleep(1/1000)
     p.stepSimulation()
+backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+k = set((backLegTouch))
+print(k)
 p.disconnect()
