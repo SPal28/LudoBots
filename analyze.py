a2b7a1096cf6026aeeef1c2af091c7b749891aef
SPal28 <ps247427@cpslions.com>
2022-07-15 08:42:27 -0400

    imported matplotlib

--- analyze.py
+++ analyze.py
@@ -1,1 +1,5 @@
-
+import numpy 
+import matplotlib.pyplot 
+numpy.load("data/backLegSensorValues.npy", mmap_mode=None, allow_pickle=False, fix_imports=True, encoding='ASCII')
+print (backLegSensorValues)
+matplotlib.pyplot.show()
