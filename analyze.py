5d8839847952a0c471be32cd82de298ed3579fb5
SPal28 <ps247427@cpslions.com>
2022-07-18 12:29:16 -0400

    e

--- analyze.py
+++ analyze.py
@@ -1,6 +1,6 @@
 import numpy 
 import matplotlib.pyplot 
 numpy.load("datavalues.npy", mmap_mode=None, allow_pickle=False, fix_imports=True, encoding='ASCII')
-plot( 'ylabel', data=backLegSensorValues)
+plot( 'ylabel', data=obj)
 print (backLegSensorValues)
 matplotlib.pyplot.show()
