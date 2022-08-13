07392352b9c8272834fa40585d8a388633c64480
SPal28 <ps247427@cpslions.com>
2022-08-13 15:33:06 -0400

    Adjusted y-axis

--- analyze.py
+++ analyze.py
@@ -10,9 +10,10 @@ pi = math.pi
 #matplotlib.pyplot.legend()
 
 targetAngleValues = numpy.load("targetAngleValues.npy")
-x = numpy.linspace(-pi/4, pi/4, 201)
+x = numpy.linspace(0, 1000, 201)
 plt.plot(x, numpy.sin(x))
 plt.xlabel('steps')
 plt.ylabel('sin(x)')
 plt.axis('tight')
+
 plt.show()
