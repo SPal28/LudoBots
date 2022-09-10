7755599ec6abb123f40c3a55d2a00b8b85b62ca6
SPal28 <ps247427@cpslions.com>
2022-09-10 09:24:10 -0400

    ith value of vector

--- simulate.py
+++ simulate.py
@@ -31,12 +31,12 @@ phaseOffset = 0
 NUM_ITR = 1000
 targetAngles = numpy.sin(numpy.linspace(0, pi * 2, NUM_ITR))
 targetAngles = targetAngles * pi/4
-targetAngles = amplitude * numpy.sin(frequency * i + phaseOffset)
+targetAngles = amplitude * numpy.sin(frequency * targetAngles[i] + phaseOffset)
 
 print(targetAngles)
-numpy.save("targetAngleValues", targetAngles, allow_pickle=True, fix_imports=True)
+# numpy.save("targetAngleValues", targetAngles, allow_pickle=True, fix_imports=True)
 
-exit()
+# exit()
 
 for i in range (1000):
     print(i)
